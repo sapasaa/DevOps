8da77e516127e4d5c27256e19dd0fd40b6a4df01
Sa Pasa <sa.pasa@fdmgroup.com>
2020-11-03 11:28:46 +1100

    changed test file location and also added a timeout

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ var server = supertest.agent("http://localhost:5000");
 
 describe("SAMPLE unit test", () => {
     it("should return home page", (done) => {
+        this.timeout(10000) // all tests in this suite get 10 seconds before timeout
+
         server
         .get("/")
         .expect("Content-type", /text/)
