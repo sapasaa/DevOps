63a14f0eaa9682aee5a313d589288c68fde7227c
Sa Pasa <sa.pasa@fdmgroup.com>
2020-11-03 11:32:33 +1100

    changed .gitignore to NOT ignore package, but package-lock and also ammended package.json

--- test/test.js
+++ test/test.js
@@ -4,12 +4,12 @@ var server = supertest.agent("http://localhost:5000");
 
 describe("SAMPLE unit test", () => {
     it("should return home page", (done) => {
-        this.timeout(10000) // all tests in this suite get 10 seconds before timeout
 
         server
         .get("/")
         .expect("Content-type", /text/)
         .expect(200)
+        .timeout(10000)
         .end(function (err,res) {
             done();
         });
